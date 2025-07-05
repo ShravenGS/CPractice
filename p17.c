1694419fdefbf3fd8e34a11c3881e9ea853872a6
Shraven GS <gsshraven@gmail.com>
2025-07-05 08:19:56 +0530

    Update p17.c

--- p17.c
+++ p17.c
@@ -14,26 +14,3 @@ int main(){
 			}
 	}
 }
-/*#include<stdio.h>
-int main(){
-	int i,j,isPrime,num;
-	printf("Enter the number:");
-	scanf("%d",&num);
-	for(i=2;i<=num;i++){
-		isPrime=1;
-		for(j=2;j<=i/2;j++){
-			if(i%j==0){
-				isPrime=0;
-				break;
-			}
-		}
-			
-	}
-	if(isPrime){
-				printf("It is a prime number");
-			}
-			else{
-			    printf("It is not a prime number");
-			}
-}
-*/
