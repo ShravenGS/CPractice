484c374fe897b6c9cc574708a7340ed7bc6ab00c
Shraven GS <gsshraven@gmail.com>
2025-07-05 08:07:58 +0530

    Update p16.c

--- p16.c
+++ p16.c
@@ -3,9 +3,8 @@ int main(){
 	int num,i,product,temp;
 	printf("Enter a number");
 	scanf("%d",&num);
-	temp=num;
 	for(i=1;i<=10;i++){
-		product=temp*i;
+		product=num*i;
 		printf("%d\n", product);
 	}
 }
