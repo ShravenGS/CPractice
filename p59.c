e05a41cdb0307aa8180cab12ef2fcf1516f1e90d
Shraven GS <gsshraven@gmail.com>
2025-07-07 15:59:10 +0530

    Create p59.c

---
+++ p59.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int a[100],i,pos,n,val;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("The elements are-->\n");
+     for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+   printf("Enter the position");
+   scanf("%d",&pos);
+   if(pos<1||pos>n+1){
+       printf("Invalid");
+       return 0;
+   }
+   pos--;
+   printf("Enter the value");
+   scanf("%d",&val);
+   for(i=n-2;i>=pos;i--){
+       a[i+1]=a[i];
+   }
+   a[pos]=val;
+    printf("\nNow the elements are-->\n");
+   for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+}
