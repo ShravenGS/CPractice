e973ebc8a6be11e3860bc1c5847241ebbca8dc40
Shraven GS <gsshraven@gmail.com>
2025-07-08 17:55:02 +0530

    Update p74.c

--- p74.c
+++ p74.c
@@ -8,6 +8,16 @@ int string_lastocc(char s[],char ch){
     }
 return count;
 }
+int string_firstocc(char s[],char ch){
+    int i;
+    for(i=0;s[i]!='\0';i++){
+        if(s[i]==ch){
+            return i;    
+        }
+    }
+    return -1;
+}
+
 int main(){
     char str1[20],ch;
     int k,n;
@@ -17,6 +27,8 @@ int main(){
     getchar();
     printf("Enter the character needed to counted in a string:");
     scanf("%c",&ch);
-     k=string_lastocc(str1,ch);
-    printf("The count of character given is:%d",k);
+    // k=string_lastocc(str1,ch);
+    k=string_firstocc(str1,ch);
+    printf("The first appearance of given char in a string:%d",k);
+    //printf("The last appearance of given char in a string:%d",k);
 }
