b8b6cadb75014ce35c87d955200e6bfa779036bf
Shraven GS <gsshraven@gmail.com>
2025-07-09 17:53:51 +0530

    Update p89.c

--- p89.c
+++ p89.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
    int x=10,y=20,z=30,i;
    int *a[3]={&x,&y,&z};
@@ -6,4 +6,15 @@ int main(){
        printf("%d ",*a[i]);
    }
 }
+*/
+#include<stdio.h>
+int main(){
+    int i;
+   int a[]={10,20,30,40,50};
+   int *p[3]={a,a+4,a+3};
+   for(i=0;i<3;i++){
+       printf("%d ",*p[i]);
+   }
+}
     
+
