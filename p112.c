45a56ecc7bcc7ff94de30e851457fbb8ea968d0f
Shraven GS <gsshraven@gmail.com>
2025-07-16 14:53:31 +0530

    Create p112.c

---
+++ p112.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+struct student{
+    int rollNo;
+    char name[50];
+    float marks;
+};
+void print(struct student s[],int size){
+    int i;
+     printf("The entered data is");
+   for(i=0;i<size;i++){
+       printf("|%d| |%s| |%.2f|\n",s[i].rollNo,s[i].name,s[i].marks);
+   }
+}
+int main(){
+   struct student s[2];
+   int i;
+   for(i=0;i<2;i++){
+       printf("Enter roll no, name and marks of the student %d:",i+1);
+       scanf("%d",&s[i].rollNo);
+       getchar();
+       scanf(" %[^\n]",&s[i].name);
+       scanf("%f",&s[i].marks);
+   }
+   print(s,2);
+  
+    return 0;
+}
