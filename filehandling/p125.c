ff6883cee4800f86062d72a0bbc04cb92d9d3b9a
Shraven GS <gsshraven@gmail.com>
2025-07-21 19:44:38 +0530

    Update p125.c

--- filehandling/p125.c
+++ filehandling/p125.c
@@ -24,5 +24,5 @@ int main(int argc,char *argv[]){
                         fclose(src);
                         fclose(dest);
                         printf("Completed\n");
-                        }
+               }
 
