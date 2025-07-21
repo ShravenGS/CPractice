61711ce81b78b9b4d1763768f20345f71068dc4e
Shraven GS <gsshraven@gmail.com>
2025-07-21 19:16:04 +0530

    Create p124.c

---
+++ filehandling/p124.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+        FILE *src,*dest;
+        char ch;
+        int line,current=1;
+        printf("Enter the number of line needed to remove");
+        scanf("%d",&line);
+        src=fopen("input.txt","r");
+        dest=fopen("output.txt","w");
+        if(src==NULL||dest==NULL){
+                printf("Cannot edit the file");
+                return 1;
+        }
+        while((ch=fgetc(src))!=EOF){
+                if(current!=line){
+                fputc(ch,dest);
+                 }
+
+                if(ch =="\n"){
+                        current++;
+                }
+        }
+        fclose(src);
+        fclose(dest);
+        printf("Completed");
+}
