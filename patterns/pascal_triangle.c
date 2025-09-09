3c3b7dfbae2e6c50df16caaaaded55ba04d1056d
Shraven GS <gsshraven@gmail.com>
2025-09-09 08:42:39 +0530

    Update pascal_triangle.c

--- patterns/pascal_triangle.c
+++ patterns/pascal_triangle.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main(){
-    int rows=5,i,j,num;
-    for(i=0;i<=rows;i++){
-        num=1;
-        for(j=0;j<=rows-i;j++){
-                printf(" ");
+    int rows=5,i,space,j,num;
+    for(i=0;i<rows;i++){
+        for(space=1;space<=rows-i;space++){
+            printf(" ");
         }
-        
+        num=1;
         for(j=0;j<=i;j++){
-            printf(" %d ",num);
+            printf("%d ",num);
             num=num*(i-j)/(j+1);
         }
-        printf("\n");
+         printf("\n");
     }
-    return 0;
+   
 }
