51d4e6ff00d3201ee520dd511039f3e172947391
Shraven GS <gsshraven@gmail.com>
2025-09-09 08:45:30 +0530

    Create pyramid_odd(*).c

---
+++ patterns/pyramid_odd(*).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int rows=5,i,space,j,num;
+    for(i=1;i<=rows;i++){
+        for(space=1;space<=rows-i;space++){
+            printf("  ");
+        }
+        for(j=1;j<=(2*i-1);j++){
+            printf("* ");
+        }
+         printf("\n");
+    }
+   
+}
