1f44cb2d4f63677b38ac6d7f9e28c7ecdf1a376b
Shraven GS <gsshraven@gmail.com>
2025-09-11 19:14:00 +0530

    Update 145.c

--- file handling/145.c
+++ file handling/145.c
@@ -1,27 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char *argv[]){
+	if(argc<3){
+		printf("./a.out usage");
+		return 1;
+	}
+	char line[1024];
+	int found=0;
+	char *pattern=argv[1];
+	int line_num=0;
+	FILE* fp=fopen(argv[2],"r");
+	if(fp==NULL){
+		printf("Source file does not exist");
+		return 1;
+	}
 
-int main() {
-    char search[100];
-    char line[1000];
-    FILE *fs;
-
-    fs = fopen("input.txt", "r");
-    if (fs == NULL) {
-        printf("Error: could not open file\n");
-        return 1;
-    }
-
-    printf("Word search: ");
-    scanf("%s", search);
-
-    while (fgets(line, sizeof(line), fs) != NULL) {
-        if (strstr(line, search) != NULL) {
-            printf("%s", line);
-        }
-    }
-
-    fclose(fs);
-    return 0;
+	while(fgets(line,sizeof(line),fp)){
+		line_num++;
+		if(strstr(line,pattern)){
+			printf("%d %s",line_num,line);
+			found=1;
+		}
+		
+		if(found!=1){
+			printf("String does not exist");
+	}
+	fclose(fp);
+}
 }
