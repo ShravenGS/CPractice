13e34922a28cbdf2ef97c7d62a2d1570924dbe26
Shraven GS <gsshraven@gmail.com>
2025-09-13 15:01:31 +0530

    Update 146.c

--- file handling/146.c
+++ file handling/146.c
@@ -1,28 +1,14 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-
-void remove_newline(char str[]){
-	int i=0;
-	while(str[i]!='\0'){
-		if(str[i]=='\n'){
-			str[i]='\0';
-			break;
-		}
-		i++;
-	}
-}
-
-void bubble_sort(char lines[][1000],int n){
+void bubble_sort(char line[][1000],int n){
 	char temp[1000];
 	int i,j;
 	for(i=0;i<n-1;i++){
-		for(j=0;j<n-i;j++){
-			if(strcmp(lines[j],lines[j+1])>0){
-				strcpy(temp,lines[j]);
-				strcpy(lines[j],lines[j+1]);
-				strcpy(lines[j+1],temp);	
-					
+		for(j=0;j<n-i-1;j++){
+			if(strcmp(line[j],line[j+1])>0){
+				strcpy(temp,line[j]);
+				strcpy(line[j],line[j+1]);
+				strcpy(line[j+1],temp);
 			}
 		}
 	}
@@ -30,30 +16,47 @@ void bubble_sort(char lines[][1000],int n){
 
 int main(){
 	FILE *fp;
+	char ch;
 	char lines[1000][1000];
-	int count=0,i;
-
-	fp=fopen("input.txt","r");
+	int count=0,i=0;
+	fp=fopen("data.txt","r");
 	if(fp==NULL){
-		printf("Cannot fetch the file");
+		printf("Cannotopen the sourcefile\n");
 		return 1;
 	}
 
+	while((ch=fgetc(fp))!=EOF){
+		if(ch=='\n'){
+			lines[count][i]='\0';
+			count++;
+			i=0;
+		}
+		else{
+			lines[count][i++]=ch;
+		}
+	}
 
-	while(fgets(lines[count],1000,fp)!=NULL && count<1000){
-		remove_newline(lines[count]);
+	if(i>0){
+		lines[count][i]='\0';
 		count++;
 	}
 
-		fclose(fp);
+	fclose(fp);
 
-		bubble_sort(lines,count);
+	bubble_sort(lines,count);
 
-		printf("\nSortedlines\n");
-		for(i=0;i<count;i++){
-			printf("%s\n",lines[i]);
-		}
+	fp=fopen("data.txt","w");
+        if(fp==NULL){
+                printf("Cannotopen the sourcefile\n");
+                return 1;
+        }
+
+	for(i=0;i<count;i++){
+		fputs(lines[i],fp);
+		fputc('\n',fp);
+	}
+
+	fclose(fp);
 
 	return 0;
 }
-
